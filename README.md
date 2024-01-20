6b9851a06ede47b064a15d066cc33616515ca312
rrumana <ryan.rumana@gmail.com>
2024-01-20 01:18:01 -0800

    Implemented multithreading, exit instructions, some error handling, and more

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Rust-Webapp
-My first start to finish webapp in Rust
+
+This is an implementation of the a Rust webserver as shown in the Rust book final chapters.
+
+This is going to be a starting off point and a template for further exploration into webapp coding.
+
+I am planning on hosting my own portfolio website which is running on rust, even if the frontend is using a more modern software stack.
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,23 @@ use std::fs;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
+
+extern crate webapp;
+use webapp::ThreadPool;
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7878")?;
 
-    for stream in listener.incoming() {
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
         match stream {
             Ok(stream) => {
-                handle_connection(stream)?;
+                pool.execute(|| {
+                    handle_connection(stream);
+                });
             }
             Err(e) => {
                 eprintln!("{}", e); 
@@ -20,10 +29,10 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    let _result = match stream.read(&mut buffer) {
+    let _ = match stream.read(&mut buffer) {
         Ok(buf) => buf,
         Err(e) => {
             eprintln!("{}", e); 
@@ -32,15 +41,19 @@ fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     };
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = 
         if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "html/index.html")
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "html/index.html")
         } else {
             ("HTTP/1.1 404 NOT FOUND", "html/404.html")
         };
 
-    let contents = fs::read_to_string(filename)?;
+    let contents = fs::read_to_string(filename).expect("Couldn't read page contents.");
 
     let response = format!{
         "{}\r\nContent-Length: {}\r\n\r\n{}",
@@ -49,8 +62,6 @@ fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
         contents
     };
     
-    stream.write_all(response.as_bytes())?;
-    stream.flush()?;
-
-    Ok(())
+    stream.write_all(response.as_bytes()).expect("Couldn't write contents of buffer.");
+    stream.flush().expect("Couldn't flush contents of buffer.");
 }
