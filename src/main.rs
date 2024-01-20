ece37f6015d79226885fbf61dd4d94af7f22a375
rrumana <ryan.rumana@gmail.com>
2024-01-19 16:55:50 -0800

    Added Error 404 page as well as more robust error handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,56 @@
+use std::fs;
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
-
-        println!("Connection established!");
+        match stream {
+            Ok(stream) => {
+                handle_connection(stream)?;
+            }
+            Err(e) => {
+                eprintln!("{}", e); 
+                std::process::exit(1)
+            },
+        }
     }
+    Ok(())
+}
 
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buffer = [0; 1024];
 
+    let _result = match stream.read(&mut buffer) {
+        Ok(buf) => buf,
+        Err(e) => {
+            eprintln!("{}", e); 
+            std::process::exit(1)
+        },
+    };
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = 
+        if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+
+    let contents = fs::read_to_string(filename)?;
+
+    let response = format!{
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    };
+    
+    stream.write_all(response.as_bytes())?;
+    stream.flush()?;
+
+    Ok(())
 }
